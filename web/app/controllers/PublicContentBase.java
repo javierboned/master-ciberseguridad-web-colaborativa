6dcee1c86d77a152edf2ab53dc4bca2ea05633af
Javier Boned <javiboned@gmail.com>
2020-04-17 02:57:40 +0200

    Arreglar registro de usuario
    No se permite el registro de un usuario cuando ya hay uno

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,11 +12,15 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
+        User u_final = User.loadUser(username);
+        if(u_final == null){
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+        }else{
+            System.out.println("Usuario ya registrado");
+        }
         registerComplete();
     }
-
     public static void registerComplete(){
         render();
     }
